44b41efc4a096d7aa99f2af33bedadc2bcd216c6
Ruben-BG <ruben321braga@gmail.com>
2023-01-16 12:39:34 -0300

    Melhora no aplicativo. Aplicativo iniciando normalmente. Ações dos botões sem funcionar.

--- app/src/main/java/com/ravtec/gestaoacademica/MainActivity.kt
+++ app/src/main/java/com/ravtec/gestaoacademica/MainActivity.kt
@@ -2,25 +2,26 @@ package com.ravtec.gestaoacademica
 
 import android.annotation.SuppressLint
 import android.content.ContentValues
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.ravtec.gestaoacademica.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    //ATRIBUTOS
-    var binding = ActivityMainBinding.inflate(layoutInflater)
-    val dbHelper = UsuariosDB(this)
+    private lateinit var binding: ActivityMainBinding
+    private val dbHelper = UsuariosDB(this)
 
-    //MÉTODOS
     @SuppressLint("Recycle")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
         val db = dbHelper.writableDatabase
+
         val valores = ContentValues().apply {
             put(UsuariosDB.UsuariosEntry.COLUMN_NAME_NOME, "Ronaldo Júnior")
             put(UsuariosDB.UsuariosEntry.COLUMN_NAME_NOMEUSUARIO, "RonaldoC")
@@ -31,16 +32,21 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
         db?.insert(UsuariosDB.UsuariosEntry.TABLE_NAME, null, valores)
 
-
+        binding.botaoProfessorAluno.setOnClickListener(this)
+        binding.botaoCoordenador.setOnClickListener(this)
         binding.botaoEntrar.setOnClickListener(this)
+        binding.botaoCadastro.setOnClickListener {
+
+        }
 
     }
 
     override fun onClick(view: View?) {
-        /*if (view!!.id == R.id.botaoEntrar) {
-
-
-
-        }*/
+        if (view!!.id == R.id.botaoCadastro) {
+            val navegarTelaCadastro = Intent(this, TelaCadastro::class.java)
+            startActivity(navegarTelaCadastro)
+        } else if (view.id == R.id.botaoEntrar) {
+            Toast.makeText(this, "lllllllllllll", Toast.LENGTH_SHORT).show()
+        }
     }
 }
